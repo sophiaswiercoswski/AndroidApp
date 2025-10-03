4c5ab479911796bf104c1c4ff7ae26ba2fafb4c7
sophiaswiercoswski <159056111+sophiaswiercoswski@users.noreply.github.com>
2025-10-02 21:05:20 -0300

    add novo campo de música favorita

--- app/src/main/java/com/ifpr/androidapptemplate/ui/usuario/PerfilUsuarioFragment.kt
+++ app/src/main/java/com/ifpr/androidapptemplate/ui/usuario/PerfilUsuarioFragment.kt
@@ -31,6 +31,9 @@ class PerfilUsuarioFragment : Fragment() {
     private lateinit var registerNameEditText: EditText
     private lateinit var registerEmailEditText: EditText
     private lateinit var registerEnderecoEditText: EditText
+
+    private lateinit var registerMusicaEditText: EditText
+
     private lateinit var registerPasswordEditText: EditText
     private lateinit var registerConfirmPasswordEditText: EditText
     private lateinit var registerButton: Button
@@ -57,6 +60,7 @@ class PerfilUsuarioFragment : Fragment() {
         registerNameEditText = view.findViewById(R.id.registerNameEditText)
         registerEmailEditText = view.findViewById(R.id.registerEmailEditText)
         registerEnderecoEditText = view.findViewById(R.id.registerEnderecoEditText)
+        registerMusicaEditText = view.findViewById(R.id.registerMusicaEditText)
         registerPasswordEditText = view.findViewById(R.id.registerPasswordEditText)
         registerConfirmPasswordEditText = view.findViewById(R.id.registerConfirmPasswordEditText)
         registerButton = view.findViewById(R.id.salvarButton)
@@ -138,6 +142,7 @@ class PerfilUsuarioFragment : Fragment() {
                     val usuario = snapshot.getValue(Usuario::class.java)
                     usuario?.let {
                         registerEnderecoEditText.setText(it.endereco ?: "")
+                        registerMusicaEditText.setText(it.musica ?: "")
                     }
                 }
             }
@@ -151,6 +156,7 @@ class PerfilUsuarioFragment : Fragment() {
     private fun updateUser() {
         val name = registerNameEditText.text.toString().trim()
         val endereco = registerEnderecoEditText.text.toString().trim()
+        val musica = registerMusicaEditText.text.toString().trim()
 
         // Acessar currentUser
         val user = auth.currentUser
@@ -158,18 +164,18 @@ class PerfilUsuarioFragment : Fragment() {
         // Verifica se o usuário atual já está definido
         if (user != null) {
             // Se o usuário já existe, atualiza os dados
-            updateProfile(user, name, endereco)
+            updateProfile(user, name, endereco, musica)
         } else {
             Toast.makeText(context, "Não foi possível encontrar o usuário logado", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun updateProfile(user: FirebaseUser?, displayName: String, endereco: String) {
+    private fun updateProfile(user: FirebaseUser?, displayName: String, endereco: String, musica: String) {
         val profileUpdates = UserProfileChangeRequest.Builder()
             .setDisplayName(displayName)
             .build()
 
-        val usuario = Usuario(user?.uid.toString() , displayName, user?.email, endereco, )
+        val usuario = Usuario(user?.uid.toString() , displayName, user?.email, endereco, musica)
 
         user?.updateProfile(profileUpdates)
             ?.addOnCompleteListener { task ->
